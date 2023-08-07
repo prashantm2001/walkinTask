ff15592c38ef5367279c2cdc49d124073bd989a7
Prashant Mishra <Prashant.Mishra@zeuslearning.mumbai>
2023-08-07 20:26:27 +0530

    work done on 7 august

--- src/screens/login/Login.tsx
+++ src/screens/login/Login.tsx
@@ -4,33 +4,11 @@ const Login = () =>{
   return (
     <div className="card">
          <div className="title">
-            <img src="src\assets/icons/back.svg" alt="go back"/>
-            <div>Log in with Quantum</div>
-            <div></div>
-         </div>
-         <div className="radios">
-            <input type="radio" id="District" checked/>
-            <label htmlFor="District">District</label>
-            <input type="radio" id="Independent school" />
-            <label htmlFor="Independent school">Independent school</label>
-         </div>
-         <div className="selector">
-            <label htmlFor="state">State*</label>
-            <select name="state" id="state">
-               <option value="District 1">Alabama</option>
-               <option value="District 1">Alabama</option>
-            </select>
-         </div>
-         <div className="selector">
-            <label htmlFor="state">District*</label>
-            <select name="state" id="state">
-               <option value="District 1">Alabama School District</option>
-               <option value="District 1">Alabama School District</option>
-            </select>
+            <div>Log in</div>
          </div>
          <div className="input" >
-            <input type="email" name="email" id="email" placeholder="Username/Email ID*"/>
-            <p>FORGOT USERNAME?</p>
+            <input type="email" name="email" id="email" placeholder="Email ID*"/>
+            <p>FORGOT Email ID?</p>
          </div>
          <div className="input" >
             <input type="password" name="password" id="password" placeholder="Password*"/>
@@ -42,6 +20,10 @@ const Login = () =>{
          </div>
          <div className="button">
             <button>LOG IN</button>
+         </div>
+         <div className='create'>
+          <p>Not registered yet?</p>
+          <h1>create an account</h1>
          </div>
       </div>
   )
